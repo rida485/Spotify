dc2f16b7744355a3527a71e9572d935273d251f6
rida485 <redha.algeriano5@gmail.com>
2024-05-27 20:58:25 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import { Figtree } from 'next/font/google'
 
 import getSongsByUserId from '@/actions/getSongsByUserId'
+import getActiveProductsWithPrices from '@/actions/getActiveProductsWithPrices'
 import Sidebar from '@/components/Sidebar'
 import ToasterProvider from '@/providers/ToasterProvider'
 import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
 import SupabaseProvider from '@/providers/SupabaseProvider'
+import Player from '@/components/Player'
 
 import './globals.css'
-import Player from '@/components/Player'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -17,13 +18,14 @@ export const metadata = {
   description: 'Spotify Clone',
 }
 
-export const revalidate = 0;
+export const revalidate = 0; //we don't want this layout to cache 
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const products = await getActiveProductsWithPrices();
   const userSongs = await getSongsByUserId();
 
   return (
@@ -32,7 +34,7 @@ export default async function RootLayout({
         <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
-            <ModalProvider />
+            <ModalProvider products={products} />
             <Sidebar songs={userSongs}>
               {children}
             </Sidebar>
